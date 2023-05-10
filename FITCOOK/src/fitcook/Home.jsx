9de55ba21ac8e42c3ed59eb7818c485f3482fd5f
hugofq <huflqu@fp.insjoaquimmir.cat>
2023-05-10 19:15:26 +0200

    Mejorado el home

--- FITCOOK/src/fitcook/Home.jsx
+++ FITCOOK/src/fitcook/Home.jsx
@@ -6,10 +6,46 @@ function Home() {
     <div className='home'>
       <h1>Home page</h1>
       <div className='himagen'>
-          <img className='imagen' src='https://corbachofit.com/wp-content/uploads/2023/02/corbacho-principal-1.png'></img>
-      </div>
-      <div className='texto'>
-        <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        {/* <img className='imagen' src='https://corbachofit.com/wp-content/uploads/2023/02/corbacho-principal-1.png'></img> */}
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
+        <div className='texto'>
+          <p>Comienza AHORA y logra de forma fácil y sin sufrir el cambio físico que siempre has querido.</p>
+        </div>
       </div>
     </div>
   );
