b2b8fa71b8bdd6dcb5b075d9a20d7e3a42135585
hugofq <huflqu@fp.insjoaquimmir.cat>
2023-05-16 20:19:11 +0200

    css distintos dependiendo el valor

--- FITCOOK/src/fitcook/Rutina.jsx
+++ FITCOOK/src/fitcook/Rutina.jsx
@@ -4,11 +4,21 @@ import './Fitcook.css'
 const Rutina = ({v}) => {
 
   console.log(v)
+
+  let dificultad = '';
+
+  if (v.level === '1') {
+    dificultad = 'carta-rutina-facil';
+  } else if (v.level === '2') {
+    dificultad = 'carta-rutina-medio';
+  } else {
+    dificultad = '  carta-rutina-dificil';
+  }
   
 
 return (
   <div key={v.id} className='rutina'>
-    <div className="carta-rutina">
+    <div className={dificultad}>
       <div className="space-y-2">
         <div className="space-y-4">
           <h4 className="text-2xl font-semibold text-cyan-900">{v.title}</h4>
