32812a0f8230ae34b942652335eccdd266662fe0
hugofq <huflqu@fp.insjoaquimmir.cat>
2023-05-16 20:03:45 +0200

    ya se ven las rutinas

--- FITCOOK/src/fitcook/Rutinas.jsx
+++ FITCOOK/src/fitcook/Rutinas.jsx
@@ -12,35 +12,26 @@ const Rutinas = () => {
   // només quan la vble d'estat refresca canvia el seu valor
   // refresca canviarà el valor quan fem alguna operació com delete   
   useEffect(() => {
-    // Crida a l'api. mètode GET
     console.log("HOLA")
-    fetch ("http://127.0.0.1:5175/api/routines",{
+    fetch ("http://127.0.0.1:8000/api/routines",{
          headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         method: "GET"
     }
     ).then( data => data.json() )
     .then (resposta => { 
-        
-        // Faria falta control·lar possible error
-            console.log("Resposta: "+resposta.data); 
-            // Actualitzem la vble d'estat rutinas
-            setRutinas(resposta.data);
-            // Canvia el valor de refresca
-            // provocarà que entri a useEffect
-            // al fer el rendertizat 
+            console.log("Resposta: "+resposta.routines); 
+            setRutinas(resposta.routines);
             setRefresca(false);
           
         } ) 
-         
+         console.log(rutinas)
   }, [refresca])   // condició d'execució del useffect
-    
-  return (
-   <>
-
   
+  return (
+   <>  
  
 <div className="py-16 bg-gradient-to-br from-green-50 to-cyan-100">  
   <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
@@ -50,7 +41,7 @@ const Rutinas = () => {
           <div className="grid gap-12 lg:grid-cols-2">
           { rutinas.map( (v,i)=> { return (
             <>
-            {  <Rutina  setRefresca={ setRefresca } key={v.id} v={v}/>  }
+            {  <Rutina  setRefresca={ setRefresca } key={v.id} v={v}/>  }   
             </>
         )})}
       </div>
